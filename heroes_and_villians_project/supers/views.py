80ae68fecd4b511cd197d0d059c6b563816f6302
Micah <airrenno@yahoo.com>
2023-01-08 03:12:39 -0600

    Created the PUT function.

--- heroes_and_villians_project/supers/views.py
+++ heroes_and_villians_project/supers/views.py
@@ -64,19 +64,28 @@ def super_list(request):
         return Response(serializer.data, status = status.HTTP_201_CREATED)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT'])
 def super_detail(request, pk):
 
-    try:
-        super = Super.objects.get(pk = pk)
+    super = get_object_or_404(Super, pk = pk)
+    # try:
+    #     super = Super.objects.get(pk = pk)
+    #     serializer = SuperSerializer(super)
+    #     return Response(serializer.data, status = status.HTTP_200_OK)
+
+    # except Super.DoesNotExist:
+    #     return Response(status = status.HTTP_204_NO_CONTENT)
+
+
+    if request.method == 'GET':
         serializer = SuperSerializer(super)
-        return Response(serializer.data, status = status.HTTP_200_OK)
+        return Response(serializer.data)
 
-    except Super.DoesNotExist:
-        return Response(status = status.HTTP_204_NO_CONTENT)
-
-
-
+    elif request.method == 'PUT':
+        serializer = SuperSerializer(super, data = request.data)
+        serializer.is_valid(raise_exception = True)
+        serializer.save()
+        return Response(serializer.data)
 
 
 
